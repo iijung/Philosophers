df9a46cf9b39eef25b8ffa5cd292cdfa60fbbaab
iijung <987kmj4@naver.com>
2023-03-30 03:24:47 +0900

    fix: errno = EINVAL;

--- philo/util/ft_atol.c
+++ philo/util/ft_atol.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 01:23:23 by minjungk          #+#    #+#             */
-/*   Updated: 2023/03/28 23:12:24 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/03/30 03:23:06 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,23 @@ static int	ft_isdigit(int c)
 	return ('0' <= c && c <= '9');
 }
 
+static long	_overflow(long num, char digit, int is_negative)
+{
+	if (num > (LONG_MAX - (digit - '0')) / 10)
+	{
+		errno = ERANGE;
+		if (is_negative)
+			return (LONG_MIN);
+		else
+			return (LONG_MAX);
+	}
+	return (0);
+}
+
 long	ft_atol(const char *str)
 {
 	long	num;
+	long	overflow;
 	int		is_negative;
 
 	num = 0;
@@ -35,18 +49,14 @@ long	ft_atol(const char *str)
 	is_negative = 0;
 	if (*str == '+' || *str == '-')
 		is_negative = (*str++ == '-');
+	if (ft_isdigit(*str))
+		errno = EINVAL;
 	while (ft_isdigit(*str))
 	{
-		if (num > (LONG_MAX - (*str - '0')) / 10)
-		{
-			errno = ERANGE;
-			if (is_negative)
-				return (LONG_MIN);
-			else
-				return (LONG_MAX);
-		}
-		num = (num * 10) + (*str - '0');
-		++str;
+		overflow = _overflow(num, *str, is_negative);
+		if (overflow)
+			return (overflow);
+		num = (num * 10) + (*str++ - '0');
 	}
 	if (is_negative)
 		return (-num);
