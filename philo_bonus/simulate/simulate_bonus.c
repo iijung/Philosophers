986493e808a9143512a327b2d9fa2d6a5bbdadac
iijung <987kmj4@naver.com>
2023-03-30 15:09:50 +0900

    refactor:

--- philo_bonus/simulate/simulate_bonus.c
+++ philo_bonus/simulate/simulate_bonus.c
@@ -6,14 +6,11 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 15:30:19 by minjungk          #+#    #+#             */
-/*   Updated: 2023/03/29 19:08:30 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/03/30 15:07:50 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "simulate_bonus.h"
-#include <stdlib.h>
-#include <string.h>
-#include <sys/wait.h>
 
 static void	_finally(struct s_simulator *simulator)
 {
@@ -107,10 +104,9 @@ int	simulate(struct s_common *common)
 
 	ret = PHILO_ERROR;
 	simulator.common = common;
-	simulator.philos = malloc(sizeof(struct s_philosopher) * num);
+	simulator.philos = ft_calloc(num, sizeof(struct s_philosopher));
 	if (simulator.philos)
 	{
-		memset(simulator.philos, 0, sizeof(struct s_philosopher) * num);
 		ret = _initial(&simulator);
 		if (ret == PHILO_INPROGRESS)
 			ret = _stop(&simulator, _start(&simulator));
--- philo_bonus/simulate/simulate_bonus.h
+++ philo_bonus/simulate/simulate_bonus.h
@@ -6,13 +6,16 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/28 15:31:20 by minjungk          #+#    #+#             */
-/*   Updated: 2023/03/29 18:31:21 by minjungk         ###   ########.fr       */
+/*   Updated: 2023/03/30 15:06:56 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SIMULATE_BONUS_H
 # define SIMULATE_BONUS_H
 # include "philosopher_bonus.h"
+# include <stdlib.h>		// malloc, free
+# include <string.h>		// memset
+# include <sys/wait.h>		// usleep
 
 struct s_simulator
 {
@@ -21,5 +24,6 @@ struct s_simulator
 	t_share					*forks;
 };
 
+extern void	*ft_calloc(size_t count, size_t size);
 extern int	simulate(struct s_common *common);
 #endif
