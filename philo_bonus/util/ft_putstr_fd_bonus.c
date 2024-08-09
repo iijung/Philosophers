902c8c650195668c846d1c981ef57c736fc34aef
iijung <987kmj4@naver.com>
2024-08-09 23:20:51 +0900

    chore: unused-result error

--- philo_bonus/util/ft_putstr_fd_bonus.c
+++ philo_bonus/util/ft_putstr_fd_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putstr_fd.c                                     :+:      :+:    :+:   */
+/*   ft_putstr_fd_bonus.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/10 00:29:45 by minjungk          #+#    #+#             */
-/*   Updated: 2023/03/26 22:55:33 by minjungk         ###   ########.fr       */
+/*   Updated: 2024/08/09 23:17:47 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,18 @@
 
 void	ft_putstr_fd(char *s, int fd)
 {
-	size_t	len;
+	ssize_t	len;
 
 	len = 0;
 	while (s && s[len])
 	{
-		if (++len == SIZE_MAX)
-		{
-			write(fd, s, len);
-			s += len;
-			len = 0;
-		}
+		if (++len < 1024)
+			continue ;
+		if (len != write(fd, s, len))
+			return ;
+		s += len;
+		len = 0;
 	}
-	write(fd, s, len);
+	if (len != write(fd, s, len))
+		return ;
 }
